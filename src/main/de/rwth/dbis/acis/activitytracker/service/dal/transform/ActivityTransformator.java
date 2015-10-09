7122b67d267c18f2a0d8b27ce1dcd38347e6ff3a
Martin <martin.hug@rwth-aachen.de>
2015-10-09 04:34:03 +0200

    DAL ActivityTransformator

---
+++ src/main/de/rwth/dbis/acis/activitytracker/service/dal/transform/ActivityTransformator.java
@@ -0,0 +1,79 @@
+package de.rwth.dbis.acis.activitytracker.service.dal.transform;
+
+import de.rwth.dbis.acis.activitytracker.service.dal.entities.Activity;
+import de.rwth.dbis.acis.activitytracker.service.dal.helpers.*;
+import de.rwth.dbis.acis.activitytracker.service.dal.helpers.DataType;
+import de.rwth.dbis.acis.activitytracker.service.dal.jooq.tables.records.ActivityRecord;
+import org.jooq.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static de.rwth.dbis.acis.activitytracker.service.dal.jooq.tables.Activity.ACTIVITY;
+
+public class ActivityTransformator implements Transformator<Activity, ActivityRecord> {
+    @Override
+    public ActivityRecord createRecord(Activity entity) {
+        ActivityRecord activityRecord = new ActivityRecord();
+        activityRecord.setCreationTime(new java.sql.Timestamp(entity.getCreationTime().getTime()));
+        activityRecord.setActivityAction(entity.getActivityAction().toString());
+        activityRecord.setDataUrl(entity.getDataUrl());
+        activityRecord.setDataType(entity.getDataType().toString());
+        activityRecord.setUserUrl(entity.getUserUrl());
+        return activityRecord;
+    }
+
+    @Override
+    public Activity mapToEntity(ActivityRecord record) {
+        return Activity.getBuilder()
+                .creationTime(record.getCreationTime())
+                .activityAction(ActivityAction.valueOf(record.getActivityAction()))
+                .dataUrl(record.getDataUrl())
+                .dataType(DataType.valueOf(record.getDataType()))
+                .userUrl(record.getUserUrl())
+                .build();
+    }
+
+    @Override
+    public Table<ActivityRecord> getTable() {
+        return ACTIVITY;
+    }
+
+    @Override
+    public TableField<ActivityRecord, Integer> getTableId() {
+        return ACTIVITY.ID;
+    }
+
+    @Override
+    public Class<? extends ActivityRecord> getRecordClass() {
+        return ActivityRecord.class;
+    }
+
+    @Override
+    public Map<Field, Object> getUpdateMap(Activity entity) {
+        return new HashMap<Field, Object>();
+    }
+
+    @Override
+    public Collection<? extends SortField<?>> getSortFields(Pageable.SortDirection sortDirection) {
+        switch (sortDirection) {
+            case DEFAULT:
+                return Arrays.asList(ACTIVITY.CREATION_TIME.asc());
+            case ASC:
+                return Arrays.asList(ACTIVITY.CREATION_TIME.asc());
+            case DESC:
+                return Arrays.asList(ACTIVITY.CREATION_TIME.desc());
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<? extends Condition> getSearchFields(String likeExpression) throws Exception {
+        return Arrays.asList(
+                ACTIVITY.ACTIVITY_ACTION.likeIgnoreCase(likeExpression)
+                        .or(ACTIVITY.DATA_TYPE.likeIgnoreCase(likeExpression))
+        );
+    }
+}
