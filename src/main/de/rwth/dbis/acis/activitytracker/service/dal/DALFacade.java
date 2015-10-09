d863fb5a3e4eaff0982674aa5c54d02c4f0a8db3
Martin <martin.hug@rwth-aachen.de>
2015-10-09 04:35:58 +0200

    DAL DAL connection handling (without pooling) and test call to database (findActivitesForUser)

--- src/main/de/rwth/dbis/acis/activitytracker/service/dal/DALFacade.java
+++ src/main/de/rwth/dbis/acis/activitytracker/service/dal/DALFacade.java
@@ -1,8 +1,15 @@
 package de.rwth.dbis.acis.activitytracker.service.dal;
 
+import de.rwth.dbis.acis.activitytracker.service.dal.entities.Activity;
+import de.rwth.dbis.acis.activitytracker.service.dal.helpers.Pageable;
+import de.rwth.dbis.acis.activitytracker.service.exception.ActivityTrackerException;
+
 import java.sql.Connection;
+import java.util.List;
 
 public interface DALFacade {
 
     Connection getConnection();
+
+    List<Activity> findActivitiesForUser(Pageable pageable) throws ActivityTrackerException;
 }
--- src/main/de/rwth/dbis/acis/activitytracker/service/dal/DALFacadeImpl.java
+++ src/main/de/rwth/dbis/acis/activitytracker/service/dal/DALFacadeImpl.java
@@ -1,19 +1,27 @@
 package de.rwth.dbis.acis.activitytracker.service.dal;
 
+import de.rwth.dbis.acis.activitytracker.service.dal.entities.Activity;
+import de.rwth.dbis.acis.activitytracker.service.dal.helpers.Pageable;
+import de.rwth.dbis.acis.activitytracker.service.dal.repositories.ActivityRepository;
+import de.rwth.dbis.acis.activitytracker.service.dal.repositories.ActivityRepositoryImpl;
+import de.rwth.dbis.acis.activitytracker.service.dal.transform.ActivityTransformator;
+import de.rwth.dbis.acis.activitytracker.service.exception.ActivityTrackerException;
 import org.jooq.*;
 import org.jooq.impl.DSL;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class DALFacadeImpl implements DALFacade {
 
     private final DSLContext dslContext;
     private final Connection connection;
-
+    private ActivityRepository activityRepository;
 
     public DALFacadeImpl(Connection connection, SQLDialect dialect) {
         this.connection = connection;
         dslContext = DSL.using(connection, dialect);
+        activityRepository = new ActivityRepositoryImpl(dslContext);
     }
 
     public DSLContext getDslContext() {
@@ -23,5 +31,10 @@ public class DALFacadeImpl implements DALFacade {
     @Override
     public Connection getConnection() {
         return connection;
+    }
+
+    @Override
+    public List<Activity> findActivitiesForUser(Pageable pageable) throws ActivityTrackerException{
+        return activityRepository.findAll();
     }
 }
