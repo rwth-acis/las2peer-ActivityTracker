8f6ab7ec2d5039e40398c5448846ae30989a0491
Martin <martin.hug@rwth-aachen.de>
2015-10-09 04:32:20 +0200

    DAL activityRepository and empty implementation

---
+++ src/main/de/rwth/dbis/acis/activitytracker/service/dal/repositories/ActivityRepository.java
@@ -0,0 +1,6 @@
+package de.rwth.dbis.acis.activitytracker.service.dal.repositories;
+
+import de.rwth.dbis.acis.activitytracker.service.dal.entities.Activity;
+
+public interface ActivityRepository extends Repository<Activity>{
+}
