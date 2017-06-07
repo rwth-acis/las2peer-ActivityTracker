0d95081ccea71736089ac9a29cc40c3ba601b9ad
Martin <martin.hug@rwth-aachen.de>
2017-06-07 03:53:59 +0200

    Removed null pointer ex

--- src/main/de/rwth/dbis/acis/activitytracker/service/dal/transform/ActivityTransformer.java
+++ src/main/de/rwth/dbis/acis/activitytracker/service/dal/transform/ActivityTransformer.java
@@ -32,7 +32,7 @@ public class ActivityTransformer implements Transformer<Activity, ActivityRecord
         activityRecord.setParentDataUrl(entity.getParentDataUrl());
         activityRecord.setParentDataType(entity.getParentDataType());
         activityRecord.setUserUrl(entity.getUserUrl());
-        activityRecord.setAdditionalObject(entity.getAdditionalObject().toString());
+        activityRecord.setAdditionalObject(entity.getAdditionalObject() == null ? null : entity.getAdditionalObject().toString());
         return activityRecord;
     }
 
