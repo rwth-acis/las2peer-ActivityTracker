772d4235b4f3b6d92b2df4d681fdc18f5303cfa1
Thore <thore@kruess.xyz>
2021-08-08 04:16:13 +0200

    Same timezone handling change

--- activity_tracker/src/main/java/de/rwth/dbis/acis/activitytracker/service/dal/entities/Activity.java
+++ activity_tracker/src/main/java/de/rwth/dbis/acis/activitytracker/service/dal/entities/Activity.java
@@ -12,7 +12,7 @@ import lombok.extern.jackson.Jacksonized;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -23,8 +23,8 @@ public class Activity extends EntityBase {
     private int id;
 
     @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime creationDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    private OffsetDateTime creationDate;
 
     @NotNull
     @Size(min = 1, max = 255)
